e3d17cd2a8c7fec20e4e65b844da42560ed67edd
rachel <impengmoumou@gmail.com>
2021-08-13 18:13:15 +0800

    build test

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: path.resolve(__dirname, "./test/index.jsx"),
+  entry: { test: path.resolve(__dirname, "./test/index.jsx") },
+  output: {
+    filename: "js/[name].[hash:8].js",
+    path: path.resolve(__dirname, "./dist/"),
+  },
   module: {
     rules: [
       {
